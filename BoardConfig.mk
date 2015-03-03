8c078029c65aa291b09ecb96723d33d727520d5a
Paul Beeler <pbeeler80@gmail.com>
2015-03-03 13:45:57 -0700

    Add SaberMod tunables
    Change-Id: I38bb2e36714164dde5f6b2def14822707ac648a6
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -153,7 +153,7 @@ MALLOC_IMPL := dlmalloc
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
 KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_SOURCE := kernel/htc/flounder
-TARGET_KERNEL_CONFIG := flounder_defconfig
+TARGET_KERNEL_CONFIG := sabermod_flounder_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_CMDLINE := androidboot.selinux=enforcing
 
